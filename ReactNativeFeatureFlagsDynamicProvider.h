ca24426c7d91ecf0b0c8096b454a3102e68b4837
Chhh <1273087648@qq.com>
2025-08-17 16:29:27 +0800

    up

--- ReactNativeFeatureFlagsDynamicProvider.h
+++ ReactNativeFeatureFlagsDynamicProvider.h
@@ -20,7 +20,7 @@
 #pragma once
 
 #include <folly/dynamic.h>
-#include <react/featureflags/ReactNativeFeatureFlagsDefaults.h>
+#include <React-featureflags/ReactNativeFeatureFlagsDefaults.h>
 
 namespace facebook::react {
 
